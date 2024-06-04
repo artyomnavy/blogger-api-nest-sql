848103dd642efce791f6417f88a8bf70e5819623
Artyom Parygin <arteme.parygin@yandex.ru>
2024-06-04 22:45:49 +0300

    added QuizQuestion entity, fix logic in create answer & connect quiz use cases, fix tests

---
+++ migrations/1717528008604-CreateTablesQuestionsQuizzesAnswersPlayersSessionsQuizzesQuestions.ts
@@ -0,0 +1,84 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTablesQuestionsQuizzesAnswersPlayersSessionsQuizzesQuestions1717528008604
+  implements MigrationInterface
+{
+  name =
+    'CreateTablesQuestionsQuizzesAnswersPlayersSessionsQuizzesQuestions1717528008604';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."quizzes_status_enum" AS ENUM('PendingSecondPlayer', 'Active', 'Finished')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "quizzes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "status" "public"."quizzes_status_enum" NOT NULL, "pair_create_date" TIMESTAMP WITH TIME ZONE NOT NULL, "start_game_date" TIMESTAMP WITH TIME ZONE, "finish_game_date" TIMESTAMP WITH TIME ZONE, "first_player_session_id" uuid, "second_player_session_id" uuid, CONSTRAINT "REL_5202a78d1a23436afe49a4ed70" UNIQUE ("first_player_session_id"), CONSTRAINT "REL_74d5ba0218da66c1953b061f62" UNIQUE ("second_player_session_id"), CONSTRAINT "PK_b24f0f7662cf6b3a0e7dba0a1b4" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "quizzes_questions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "quiz_id" uuid NOT NULL, "question_id" uuid NOT NULL, "index" integer NOT NULL, CONSTRAINT "PK_95355468d0853bfecdbdfb961b1" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "questions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "body" character varying(500) COLLATE "C" NOT NULL, "correct_answers" character varying array COLLATE "C" NOT NULL, "published" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_08a6d4b0f49ff300bf3a0ca60ac" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."answers_answer_status_enum" AS ENUM('Correct', 'Incorrect')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "answers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "body" character varying COLLATE "C" NOT NULL, "answer_status" "public"."answers_answer_status_enum" NOT NULL, "added_at" TIMESTAMP WITH TIME ZONE NOT NULL, "player_session_id" uuid, "question_id" uuid, CONSTRAINT "PK_9c32cec6c71e06da0254f2226c6" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "players_sessions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "score" integer NOT NULL DEFAULT '0', "player_id" uuid, CONSTRAINT "PK_b4ea32ed842189e1804a82d1e1f" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes" ADD CONSTRAINT "FK_5202a78d1a23436afe49a4ed70c" FOREIGN KEY ("first_player_session_id") REFERENCES "players_sessions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes" ADD CONSTRAINT "FK_74d5ba0218da66c1953b061f625" FOREIGN KEY ("second_player_session_id") REFERENCES "players_sessions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes_questions" ADD CONSTRAINT "FK_2db1c227456ea298f25530a00e9" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes_questions" ADD CONSTRAINT "FK_4a4f46287278c35a92a8466fe1a" FOREIGN KEY ("quiz_id") REFERENCES "quizzes"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "answers" ADD CONSTRAINT "FK_836e9aa54363f2be82c5885c8d9" FOREIGN KEY ("player_session_id") REFERENCES "players_sessions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "answers" ADD CONSTRAINT "FK_677120094cf6d3f12df0b9dc5d3" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "players_sessions" ADD CONSTRAINT "FK_9dbb979dec9dc7d5967fe7d5bdb" FOREIGN KEY ("player_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "players_sessions" DROP CONSTRAINT "FK_9dbb979dec9dc7d5967fe7d5bdb"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "answers" DROP CONSTRAINT "FK_677120094cf6d3f12df0b9dc5d3"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "answers" DROP CONSTRAINT "FK_836e9aa54363f2be82c5885c8d9"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes_questions" DROP CONSTRAINT "FK_4a4f46287278c35a92a8466fe1a"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes_questions" DROP CONSTRAINT "FK_2db1c227456ea298f25530a00e9"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes" DROP CONSTRAINT "FK_74d5ba0218da66c1953b061f625"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "quizzes" DROP CONSTRAINT "FK_5202a78d1a23436afe49a4ed70c"`,
+    );
+    await queryRunner.query(`DROP TABLE "players_sessions"`);
+    await queryRunner.query(`DROP TABLE "answers"`);
+    await queryRunner.query(`DROP TYPE "public"."answers_answer_status_enum"`);
+    await queryRunner.query(`DROP TABLE "questions"`);
+    await queryRunner.query(`DROP TABLE "quizzes_questions"`);
+    await queryRunner.query(`DROP TABLE "quizzes"`);
+    await queryRunner.query(`DROP TYPE "public"."quizzes_status_enum"`);
+  }
+}
