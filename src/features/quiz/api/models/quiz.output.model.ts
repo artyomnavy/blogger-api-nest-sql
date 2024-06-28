f165f0589fa9a9f01f1bc8db46a93db53e8cdbc4
Artyom Parygin <arteme.parygin@yandex.ru>
2024-06-28 14:23:00 +0300

    fix query typeorm for getAllQuizzes

--- src/features/quiz/api/models/quiz.output.model.ts
+++ src/features/quiz/api/models/quiz.output.model.ts
@@ -1,4 +1,4 @@
-import { QuizStatuses } from '../../../../common/utils';
+import { AnswerStatuses, QuizStatuses } from '../../../../common/utils';
 import { PlayerSessionOutputQuizModel } from './player-session.output.model';
 import { QuestionOutputQuizModel } from './question.output.model';
 
@@ -11,4 +11,34 @@ export class QuizOutputModel {
   pairCreatedDate: string;
   startGameDate: string | null;
   finishGameDate: string | null;
+}
+
+export class QuizMapperModel {
+  id: string;
+  status: QuizStatuses;
+  pairCreatedDate: Date;
+  startGameDate: Date;
+  finishGameDate: Date;
+  sessionIdPlayer1: string;
+  scorePlayer1: number;
+  idPlayer1: string;
+  loginPlayer1: string;
+  sessionIdPlayer2: string;
+  scorePlayer2: number;
+  idPlayer2: string;
+  loginPlayer2: string;
+  questions: {
+    id: string;
+    body: string;
+  }[];
+  answersPlayer1: {
+    questionId: string;
+    answerStatus: AnswerStatuses;
+    addedAt: Date;
+  }[];
+  answersPlayer2: {
+    questionId: string;
+    answerStatus: AnswerStatuses;
+    addedAt: Date;
+  }[];
 }
--- src/features/quiz/infrastructure/quizzes.query-repository.ts
+++ src/features/quiz/infrastructure/quizzes.query-repository.ts
@@ -3,8 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Quiz } from '../domain/quiz.entity';
 import { QuizStatuses } from '../../../common/utils';
-import { QuizOutputModel } from '../api/models/quiz.output.model';
+import {
+  QuizOutputModel,
+  QuizMapperModel,
+} from '../api/models/quiz.output.model';
 import { PaginatorModel } from '../../../common/models/paginator.input.model';
+import { PaginatorOutputModel } from '../../../common/models/paginator.output.model';
 
 @Injectable()
 export class QuizzesQueryRepository {
@@ -22,7 +26,7 @@ export class QuizzesQueryRepository {
       | 'searchLoginTerm'
       | 'searchEmailTerm'
     >,
-  ) {
+  ): Promise<PaginatorOutputModel<QuizOutputModel>> {
     const pageNumber = queryData.pageNumber ? +queryData.pageNumber : 1;
     const pageSize = queryData.pageSize ? +queryData.pageSize : 10;
     const sortBy = queryData.sortBy ? queryData.sortBy : 'pairCreatedDate';
@@ -30,60 +34,61 @@ export class QuizzesQueryRepository {
       ? (queryData.sortDirection.toUpperCase() as 'ASC' | 'DESC')
       : 'DESC';
 
-    // TO DO: fix query (why incorrect response questions[] and answers[])
     const quizzes = await this.quizQueryRepository
       .createQueryBuilder('qz')
       .select([
         // quiz
-        'qz.id',
-        'qz.status',
-        'qz.pairCreatedDate',
-        'qz.startGameDate',
-        'qz.finishGameDate',
+        'qz.id AS "id"',
+        'qz.status AS "status"',
+        'qz.pairCreatedDate AS "pairCreatedDate"',
+        'qz.startGameDate AS "startGameDate"',
+        'qz.finishGameDate AS "finishGameDate"',
         // playerSession for first player
-        'fps.id',
-        'fps.score',
+        'fps.id AS "sessionIdPlayer1"',
+        'fps.score AS "scorePlayer1"',
         // user info for first player
-        'fpu.id',
-        'fpu.login',
-        // answers for first player
-        'fpa.id',
-        'fpa.body',
-        'fpa.answerStatus',
-        'fpa.addedAt',
-        // question for first player
-        'fpaq.id AS questionId',
+        'fpu.id AS "idPlayer1"',
+        'fpu.login AS "loginPlayer1"',
         // playerSession for second player
-        'sps.id',
-        'sps.score',
+        'sps.id AS "sessionIdPlayer2"',
+        'sps.score AS "scorePlayer2"',
         // user info for second player
-        'spu.id',
-        'spu.login',
-        // answers for second player
-        'spa.id',
-        'spa.body',
-        'spa.answerStatus',
-        'spa.addedAt',
-        // question for second player
-        'spaq.id AS questionId',
-        // quizzes and questions for quiz
-        'qzq.id',
-        'qzq.index',
-        // questions for quiz
-        'q.id',
-        'q.body',
-        'q.correctAnswers',
+        'spu.id AS "idPlayer2"',
+        'spu.login AS "loginPlayer2"',
       ])
+      // Подзапрос массива вопросов игры
+      .addSelect((subQuery) => {
+        return subQuery
+          .select("json_agg(json_build_object('id', q.id, 'body', q.body))")
+          .from('quizzes_questions', 'qzq')
+          .leftJoin('questions', 'q', 'qzq.question_id = q.id')
+          .where('qzq.quiz_id = qz.id');
+      }, 'questions')
+      // Подзапрос массива ответов первого игрока
+      .addSelect((subQuery) => {
+        return subQuery
+          .select(
+            "json_agg(json_build_object('questionId', q.id, 'answerStatus', a.answerStatus, 'addedAt', a.addedAt) ORDER BY a.addedAt ASC)",
+          )
+          .from('answers', 'a')
+          .leftJoin('questions', 'q', 'a.question_id = q.id')
+          .where('a.player_session_id = fps.id');
+      }, 'answersPlayer1')
+      // Подзапрос массива ответов второго игрока
+      .addSelect((subQuery) => {
+        return subQuery
+          .select(
+            "json_agg(json_build_object('questionId', q.id, 'answerStatus', a.answerStatus, 'addedAt', a.addedAt) ORDER BY a.addedAt ASC)",
+          )
+          .from('answers', 'a')
+          .leftJoin('questions', 'q', 'a.question_id = q.id')
+          .where('a.player_session_id = sps.id');
+      }, 'answersPlayer2')
+      // Продолжение основного запроса
       .leftJoin('qz.firstPlayerSession', 'fps')
       .leftJoin('fps.player', 'fpu')
-      .leftJoin('fps.answers', 'fpa')
-      .leftJoin('fpa.question', 'fpaq')
       .leftJoin('qz.secondPlayerSession', 'sps')
       .leftJoin('sps.player', 'spu')
-      .leftJoin('sps.answers', 'spa')
-      .leftJoin('spa.question', 'spaq')
-      .leftJoin('qz.quizQuestion', 'qzq')
-      .leftJoin('qzq.question', 'q')
       .where('(fpu.id = :playerId OR spu.id = :playerId)', {
         playerId,
       })
@@ -91,7 +96,7 @@ export class QuizzesQueryRepository {
       .addOrderBy('qz.pairCreatedDate', 'DESC')
       .offset((pageNumber - 1) * pageSize)
       .limit(pageSize)
-      .getMany();
+      .getRawMany();
 
     const totalCount: number = await this.quizQueryRepository
       .createQueryBuilder('qz')
@@ -112,7 +117,7 @@ export class QuizzesQueryRepository {
       page: pageNumber,
       pageSize: pageSize,
       totalCount: totalCount,
-      items: await Promise.all(quizzes.map((quiz) => this.quizMapper(quiz))),
+      items: await Promise.all(quizzes.map((quiz) => this.quizRawMapper(quiz))),
     };
   }
   async getQuizByPlayerIdAndPendingOrActiveStatusForConnection(
@@ -389,6 +394,61 @@ export class QuizzesQueryRepository {
             return {
               id: quizQuestion.question.id,
               body: quizQuestion.question.body,
+            };
+          })
+        : null,
+      status: quiz.status,
+      pairCreatedDate: quiz.pairCreatedDate.toISOString(),
+      startGameDate: quiz.startGameDate
+        ? quiz.startGameDate.toISOString()
+        : null,
+      finishGameDate: quiz.finishGameDate
+        ? quiz.finishGameDate.toISOString()
+        : null,
+    };
+  }
+  async quizRawMapper(quiz: QuizMapperModel): Promise<QuizOutputModel> {
+    return {
+      id: quiz.id,
+      firstPlayerProgress: {
+        answers: quiz.answersPlayer1
+          ? quiz.answersPlayer1.map((answer) => {
+              return {
+                questionId: answer.questionId,
+                answerStatus: answer.answerStatus,
+                addedAt: new Date(answer.addedAt).toISOString(),
+              };
+            })
+          : [],
+        player: {
+          id: quiz.idPlayer1,
+          login: quiz.loginPlayer1,
+        },
+        score: quiz.scorePlayer1,
+      },
+      secondPlayerProgress: quiz.sessionIdPlayer2
+        ? {
+            answers: quiz.answersPlayer2
+              ? quiz.answersPlayer2.map((answer) => {
+                  return {
+                    questionId: answer.questionId,
+                    answerStatus: answer.answerStatus,
+                    addedAt: new Date(answer.addedAt).toISOString(),
+                  };
+                })
+              : [],
+            player: {
+              id: quiz.idPlayer2,
+              login: quiz.loginPlayer2,
+            },
+            score: quiz.scorePlayer2,
+          }
+        : null,
+      questions: quiz.sessionIdPlayer2
+        ? quiz.questions.map((question) => {
+            return {
+              id: question.id,
+              body: question.body,
             };
           })
         : null,
